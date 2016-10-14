0375189dde368731ea12d798bd7767f25e09809d
fabdin <fabdin@express.com>
2016-10-14 08:52:43 -0400

    Enabling Swagger

--- src/main/java/com/sogeti/smartshelf/web/MainController.java
+++ src/main/java/com/sogeti/smartshelf/web/MainController.java
@@ -4,20 +4,20 @@ import com.sogeti.smartshelf.model.Product;
 import com.sogeti.smartshelf.model.Shelf;
 import com.sogeti.smartshelf.model.UserDoc;
 import com.sogeti.smartshelf.service.DataService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import javax.servlet.http.HttpSession;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
-import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiParam;
 import java.util.List;
 import javax.ws.rs.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -68,7 +68,7 @@ public class MainController {
     
     @RequestMapping(value = "/shelf", method = RequestMethod.PUT)
     @ApiOperation(value = "Add new shelf", produces = "application/json")
-    public ResponseEntity addShelf() {
+    public ResponseEntity addShelf(@RequestBody Shelf shelf) {
         
         return new ResponseEntity(HttpStatus.OK);
     }    
@@ -77,7 +77,7 @@ public class MainController {
     
     @RequestMapping(value = "/shelf", method = RequestMethod.POST)
     @ApiOperation(value = "Update shelf", produces = "application/json")
-    public ResponseEntity updateShelf() {
+    public ResponseEntity updateShelf(@RequestBody Shelf shelf) {
         
         return new ResponseEntity(HttpStatus.OK);
     }    
