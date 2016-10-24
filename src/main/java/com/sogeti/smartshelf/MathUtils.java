e1adca16972858d97de202795631ed846e809452
Fadi Abdin <fadiabdeen@gmail.com>
2016-10-24 06:33:03 -0400

    calculate real weight by gram from pressure

--- src/main/java/com/sogeti/smartshelf/MathUtils.java
+++ src/main/java/com/sogeti/smartshelf/MathUtils.java
@@ -11,12 +11,39 @@ public class MathUtils {
     
     public static Integer getPersentage(Product product, Integer currentWeight){
         
+        if(product == null) return 0;
         Integer weightWithoutPackaging = product.getWeight() - product.getPackageWeight();
         Integer currentWeightWithoutPackaging = currentWeight - product.getPackageWeight();
         
         Integer persentage = (currentWeightWithoutPackaging/weightWithoutPackaging)*100;
         
         return persentage;
+    }
+    
+    public static Integer normalizeWeight(String pressure, String name){
+        
+        Double d=new Double(pressure);
+        
+        
+        if(d>500000){
+            d=0.00;
+        }
+        
+        Double result = 0.0;
+        
+        switch (name) {
+            case "scale1":
+                result=d*3.48;
+                
+                break;
+            case "scale2":
+                result=d*3.82;
+                break;
+        }
+        
+         
+
+        return result.intValue();
     }
     
 }
