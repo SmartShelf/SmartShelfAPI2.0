0d300e8f053b8489c22b4646278fa6c3d3b47d50
Jason Main <jason_main@abercrombie.com>
2017-03-12 13:22:38 -0400

    added endpoint for scoring shelf data in data model; added code to lookup product data from UserDoc by username and productId

--- src/main/java/com/sogeti/smartshelf/web/PredictionController.java
+++ src/main/java/com/sogeti/smartshelf/web/PredictionController.java
@@ -9,13 +9,17 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.sogeti.smartshelf.model.DataModel;
-import com.sogeti.smartshelf.service.PredictionService;
+import com.sogeti.smartshelf.model.Scale;
+import com.sogeti.smartshelf.model.Shelf;
+import com.sogeti.smartshelf.model.UserDoc;
+import com.sogeti.smartshelf.service.DataService;
 
 /**
 *
@@ -26,7 +30,14 @@ import com.sogeti.smartshelf.service.PredictionService;
 public class PredictionController {
     
 	@Autowired
-	PredictionService predictionService;
+	DataService dataService;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<String> testPredictionService() {
+		String testMessage = "test Message";
+		
+		return new ResponseEntity<String>(testMessage, HttpStatus.OK);
+	}
 	
 	@RequestMapping(value = "/models", method = RequestMethod.GET)
 	public ResponseEntity<String> getDeployedModels() {
@@ -44,10 +55,47 @@ public class PredictionController {
 		return new ResponseEntity<String>("testing getDeployModels()", HttpStatus.OK);
 	}
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<String> testPredictionService() {
-		String testMessage = "test Message";
+	@RequestMapping(value = "/user/{username}/product/{productId}",
+			method = RequestMethod.GET)
+	public ResponseEntity<String> predictProduct(@PathVariable String username,
+			@PathVariable String productId) {
+		UserDoc userDoc = dataService.findUser(username);
 		
-		return new ResponseEntity<String>(testMessage, HttpStatus.OK);
+		String usernameFromDoc = userDoc.getUser().getUsername();
+		String productIdFromDoc = "";
+		Integer productWeightFromDoc = null;
+		List<Shelf> shelfs = userDoc.getShelfs();
+		for (Shelf shelf : shelfs) {
+			boolean productIdFound = false;
+			List<Scale> scales = shelf.getScales();
+			for (Scale scale : scales) {
+				String scaleProductId = scale.getProductId(); 
+				if (scaleProductId.equalsIgnoreCase(productId)) {
+					productIdFromDoc = scaleProductId;
+					productWeightFromDoc = scale.getWeight();
+					productIdFound = true;
+					break;
+				}
+			}
+			if (productIdFound) {
+				break;
+			}
+		}
+		
+		String body;
+		if (productId.length() > 0) {
+			body = "username=" + usernameFromDoc +
+					", productId=" + productIdFromDoc +
+					", weight=" + productWeightFromDoc;
+		}
+		else {
+			body = "product not found for user";
+		}
+		
+		ResponseEntity<String> response = 
+				new ResponseEntity<String>(body, HttpStatus.OK);
+		
+		return response;
 	}
+	
 }
