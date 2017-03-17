5849d913264a7a08377c10389b0751b16d99a06b
Jason Main <jason_main@abercrombie.com>
2017-03-17 00:06:23 -0400

    changes for getting scoring data back from Watson ML

--- src/main/java/com/sogeti/smartshelf/model/ProductScoringCriteria.java
+++ src/main/java/com/sogeti/smartshelf/model/ProductScoringCriteria.java
@@ -1,5 +1,11 @@
 package com.sogeti.smartshelf.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ProductScoringCriteria {
 
 	private Integer finalPercentage;
@@ -11,76 +17,123 @@ public class ProductScoringCriteria {
 	private Integer householdSize;
 	private Integer zipCode;
 	
-	public Integer getFinalPercentage() {
-		return finalPercentage;
-	}
+	private String tablename;
+	private List<String> header;
+	private List<List<Object>> data;
 	
-	public void setFinalPercentage(Integer finalPercentage) {
-		this.finalPercentage = finalPercentage;
+	public ProductScoringCriteria() {
+		tablename = "input.csv";
+		header = new ArrayList<String>();
+		header.add("finalPercentage");
+		header.add("finalWeight");
+		header.add("startingWeight");
+		header.add("useDays");
+		header.add("productId");
+		header.add("avgAge");
+		header.add("householdSize");
+		header.add("zipCode");
+		data = new ArrayList<>();
 	}
+
+//	": [[20.0,7.0,35,4,600,"40.5",2,43235]]
 	
-	public Integer getFinalWeight() {
-		return finalWeight;
-	}
+//	"tablename" : "input.csv",
+//	"header": ["finalPercentage","finalWeight","startingWeight","useDays","productId","avgAge","householdSize","zipCode"]
 	
-	public void setFinalWeight(Integer finalWeight) {
-		this.finalWeight = finalWeight;
+	public String getTablename() {
+		return tablename;
 	}
-	
-	public Integer getStartingWeight() {
-		return startingWeight;
+
+	public void setTablename(String tablename) {
+		this.tablename = tablename;
 	}
-	
-	public void setStartingWeight(Integer startingWeight) {
-		this.startingWeight = startingWeight;
+
+	public List<String> getHeader() {
+		return header;
 	}
-	
-	public Integer getUseDays() {
-		return useDays;
+
+	public void setHeader(List<String> header) {
+		this.header = header;
 	}
-	
-	public void setUseDays(Integer useDays) {
-		this.useDays = useDays;
+
+	public List<List<Object>> getData() {
+		return data;
 	}
-	
-	public String getProductId() {
-		return productId;
+
+	public void setData(List<List<Object>> data) {
+		this.data = data;
 	}
-	
-	public void setProductId(String productId) {
-		this.productId = productId;
-	}
-	
-	public Double getAvgAge() {
-		return avgAge;
-	}
-	
-	public void setAvgAge(Double avgAge) {
-		this.avgAge = avgAge;
-	}
-	
-	public Integer getHouseholdSize() {
-		return householdSize;
-	}
-	
-	public void setHouseholdSize(Integer householdSize) {
-		this.householdSize = householdSize;
-	}
-	
-	public int getZipCode() {
-		return zipCode;
-	}
-	
-	public void setZipCode(Integer zipCode) {
-		this.zipCode = zipCode;
-	}
-	
-	@Override
-	public String toString() {
-		return "ProductScoringCriteria [finalPercentage=" + finalPercentage
-				+ ", finalWeight=" + finalWeight + ", startingWeight="
-				+ startingWeight + ", useDays=" + useDays + ", productId="
-				+ productId + ", avgAge=" + avgAge + ", householdSize="
-				+ householdSize + ", zipCode=" + zipCode + "]";
-	}
+
+//	public Integer getFinalPercentage() {
+//		return finalPercentage;
+//	}
+//	
+//	public void setFinalPercentage(Integer finalPercentage) {
+//		this.finalPercentage = finalPercentage;
+//	}
+//	
+//	public Integer getFinalWeight() {
+//		return finalWeight;
+//	}
+//	
+//	public void setFinalWeight(Integer finalWeight) {
+//		this.finalWeight = finalWeight;
+//	}
+//	
+//	public Integer getStartingWeight() {
+//		return startingWeight;
+//	}
+//	
+//	public void setStartingWeight(Integer startingWeight) {
+//		this.startingWeight = startingWeight;
+//	}
+//	
+//	public Integer getUseDays() {
+//		return useDays;
+//	}
+//	
+//	public void setUseDays(Integer useDays) {
+//		this.useDays = useDays;
+//	}
+//	
+//	public String getProductId() {
+//		return productId;
+//	}
+//	
+//	public void setProductId(String productId) {
+//		this.productId = productId;
+//	}
+//	
+//	public Double getAvgAge() {
+//		return avgAge;
+//	}
+//	
+//	public void setAvgAge(Double avgAge) {
+//		this.avgAge = avgAge;
+//	}
+//	
+//	public Integer getHouseholdSize() {
+//		return householdSize;
+//	}
+//	
+//	public void setHouseholdSize(Integer householdSize) {
+//		this.householdSize = householdSize;
+//	}
+//	
+//	public int getZipCode() {
+//		return zipCode;
+//	}
+//	
+//	public void setZipCode(Integer zipCode) {
+//		this.zipCode = zipCode;
+//	}
+//	
+//	@Override
+//	public String toString() {
+//		return "ProductScoringCriteria [finalPercentage=" + finalPercentage
+//				+ ", finalWeight=" + finalWeight + ", startingWeight="
+//				+ startingWeight + ", useDays=" + useDays + ", productId="
+//				+ productId + ", avgAge=" + avgAge + ", householdSize="
+//				+ householdSize + ", zipCode=" + zipCode + "]";
+//	}
 }
--- src/main/java/com/sogeti/smartshelf/web/PredictionController.java
+++ src/main/java/com/sogeti/smartshelf/web/PredictionController.java
@@ -1,20 +1,30 @@
 package com.sogeti.smartshelf.web;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sogeti.smartshelf.model.DataModel;
 import com.sogeti.smartshelf.model.Member;
 import com.sogeti.smartshelf.model.Product;
@@ -61,16 +71,90 @@ public class PredictionController {
 	@RequestMapping(value = "/user/{username}/shelf/{shelfId}/scale/{scaleId}",
 			method = RequestMethod.GET)
 	public ResponseEntity<String> predictProduct(@PathVariable String username,
-			@PathVariable String shelfId, @PathVariable String scaleId) {
+			@PathVariable String shelfId, @PathVariable String scaleId) throws JsonParseException, JsonMappingException, IOException {
 		ProductScoringCriteria criteria = getProductScoringCriteria(username, shelfId, scaleId);
+		String body = "";
+		Double outOfStockProbability = predictProductUse(criteria);
+		
+		if (evaluateOutOfStockProbability(outOfStockProbability)) {
+			body = "{showNotification: true}";
+		}
+		else {
+			body = "{showNotification: false}";
+		}
+		
 //		String criteriaHeaders = getCSVHeaders(criteria);
 //		String criteriaCSV = transformProductScoringCriteriaToCSV(criteria);
 		
-		String body = criteria.toString();
-		ResponseEntity<String> response = 
-				new ResponseEntity<String>(body, HttpStatus.OK);
+		
+//		ResponseEntity<List<DataModel>> dataModelsResponse = restTemplate.exchange(
+//				url, HttpMethod.GET, null, 
+//				new ParameterizedTypeReference<List<DataModel>>(){}, vars);
+		ResponseEntity<String> response = new ResponseEntity<String>(body, HttpStatus.OK);
 		
 		return response;
+	}
+
+	private Double predictProductUse(ProductScoringCriteria criteria) {
+//      {
+//		"tablename" : "input.csv",
+//		"header": ["finalPercentage","finalWeight","startingWeight","useDays","productId","avgAge","householdSize","zipCode"],
+//		"data": [[20.0,7.0,35,4,600,"40.5",2,43235]]
+//		}
+//		StringBuilder
+//		RestTemplate restTemplate = new RestTemplate();
+//		String url = "https://ibm-watson-ml.mybluemix.net/pm/v1/score/Smart_Shelf_both_v1.4";
+//		String accesskey = "iqMbzXCtPD/WMQikw8DJWjSVoHWH/UTNlyfhgq9LOgJXkrv6HHH+pi7UhJdoYpSwHxGxQ3pIogjgEOjN0TGDTcL0h32gVzPkwMbmHXNpi+FQYUqQmv73SQJrb1WXWeZv";
+//		String copy=       "iqMbzXCtPD/WMQikw8DJWjSVoHWH/UTNlyfhgq9LOgJXkrv6HHH+pi7UhJdoYpSwHxGxQ3pIogjgEOjN0TGDTcL0h32gVzPkwMbmHXNpi+FQYUqQmv73SQJrb1WXWeZv";
+//		Map<String, String> vars = new HashMap<>();
+//		vars.put("accesskey", accesskey);
+//		
+//		ResponseEntity<String> response = restTemplate.postForEntity(url, criteria, String.class, vars);
+		
+		RestTemplate restTemplate = new RestTemplate();
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+		
+		String url = "https://ibm-watson-ml.mybluemix.net/pm/v1/score/Smart_Shelf_both_v1.4";
+		String accesskey = "iqMbzXCtPD/WMQikw8DJWjSVoHWH/UTNlyfhgq9LOgJXkrv6HHH+pi7UhJdoYpSwHxGxQ3pIogjgEOjN0TGDTcL0h32gVzPkwMbmHXNpi+FQYUqQmv73SQJrb1WXWeZv";
+		
+		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url)
+		        .queryParam("accesskey", accesskey);
+		
+		HttpEntity<?> entity = new HttpEntity<ProductScoringCriteria>(criteria, headers);
+		
+		HttpEntity<String> response = restTemplate.exchange(
+		        builder.build().encode().toUri(), 
+		        HttpMethod.POST, 
+		        entity, 
+		        String.class);
+
+		String responseBody = response.getBody();
+		ObjectMapper mapper = new ObjectMapper();
+		Double outOfStockProbability = -1.0;
+		try {
+			ProductScoringCriteria[] productScoringArray = mapper.readValue(responseBody, ProductScoringCriteria[].class);
+			List<ProductScoringCriteria> productScoringList = new ArrayList<ProductScoringCriteria>(Arrays.asList(productScoringArray));
+			ProductScoringCriteria productScoring = productScoringList.get(0);
+			List<List<Object>> scoringDataList = productScoring.getData();
+			List<Object> scoringData = scoringDataList.get(0);
+			outOfStockProbability = (Double) scoringData.get(11);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return outOfStockProbability;
+	}
+	
+	private Boolean evaluateOutOfStockProbability(Double outOfStockProbability) {
+		if (outOfStockProbability >= 0.8) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 	
 	private ProductScoringCriteria getProductScoringCriteria(String username,
@@ -78,24 +162,36 @@ public class PredictionController {
 		UserDoc userDoc = dataService.findUser(username);
 		List<Scale> scales = dataService.getScales(shelfId);
 		ProductScoringCriteria criteria = new ProductScoringCriteria();
-		
+		List<Object> scoringData = new ArrayList<>();
 		for (Scale scale : scales) {
 			if (scale.getId().equalsIgnoreCase(scaleId)) {
 				String scaleProductId = scale.getProductId();
 				Product product = dataService.getProduct(scaleProductId);
-
 				dataService.populatePersentageInScale(scale);
-				criteria.setFinalPercentage(scale.getPersentage());
-				criteria.setStartingWeight(product.getWeight());
-				criteria.setFinalWeight(scale.getWeight());
-				criteria.setUseDays(scale.getUseDays());
-				criteria.setProductId(scaleProductId);
+//				[20.0,7.0,35,4,600,"40.5",2,43235]
+				scoringData.add(20.0);
+				scoringData.add(7.0);
+				scoringData.add(35);
+				scoringData.add(4);
+				scoringData.add(600);
 				List<Member> householdMembers =
 						userDoc.getUser().getHousehold().getMembers();
 				Double averageAge = calculateAverageAge(householdMembers);
-				criteria.setAvgAge(averageAge);
-				criteria.setHouseholdSize(householdMembers.size());
-				criteria.setZipCode(userDoc.getUser().getZipCode());
+				scoringData.add(40.5);
+				scoringData.add(2);
+				scoringData.add("43235");
+				
+				criteria.getData().add(scoringData);
+				
+//				criteria.setFinalPercentage(scale.getPersentage());
+//				criteria.setStartingWeight(product.getWeight());
+//				criteria.setFinalWeight(scale.getWeight());
+//				criteria.setUseDays(scale.getUseDays());
+//				criteria.setProductId(scaleProductId);
+//				
+//				criteria.setAvgAge(averageAge);
+//				criteria.setHouseholdSize(householdMembers.size());
+//				criteria.setZipCode(userDoc.getUser().getZipCode());
 			}
 		}
 		
